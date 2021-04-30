66adcb0ea4773724f7ef151eac64d865fefca69b
Darya Kolosova <shekoldina.dasha@gmail.com>
2021-04-30 13:07:54 +0300

    feat: добавить функциюgetCard() для просмотра модального окна, удаления и лайков

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const popupEdit = document.querySelector('.popup_type_edit');
 const popupEditBtnClose = popupEdit.querySelector('.popup__btn-close');
 const popupName = popupEdit.querySelector('input[name="profile-name"]');
 const popupHobby = popupEdit.querySelector('input[name="profile-hobby"]');
-const popupForm = popupEdit.querySelector('.popup__form');
+const popupEditForm = popupEdit.querySelector('.popup__form');
 const profile = document.querySelector('.profile');
 const profileName = profile.querySelector('.profile__name');
 const profileHobby = profile.querySelector('.profile__hobby');
@@ -12,56 +12,126 @@ const popupPhotoTemplate = document.querySelector('#popup_type_photo').content;
 const popupPhotoElement = popupPhotoTemplate.querySelector('.popup_type_photo').cloneNode(true);
 const popupPhotoBtnClose = popupPhotoElement.querySelector('.popup__btn-close');
 
-const elementImages = document.querySelectorAll('.element__image');
-const likeElementButtons = document.querySelectorAll('.element__like');
+
+const addButton = document.querySelector('.profile__button_type_add');
+const popupAdd = document.querySelector('.popup_type_add');
+const popupAddBtnClose = popupAdd.querySelector('.popup__btn-close');
+const popupAddForm = popupAdd.querySelector('.popup__form');
+
+const elementTemplate = document.querySelector('#element').content;
+const elementContainer = document.querySelector('.elements__list');
+
+const initialCards = [
+  {
+    name: 'Castel San Pietro',
+    link: './images/element-castel-san-pietro.jpeg'
+  },
+  {
+    name: 'Giardino Bardini',
+    link: './images/element-giardino-bardini.jpeg'
+  },
+  {
+    name: 'Manarola',
+    link: './images/element-manarola.jpeg'
+  },
+  {
+    name: 'Naples',
+    link: './images/element-naples.jpeg'
+  },
+  {
+    name: 'Rialto Bridge',
+    link: './images/element-rialto-bridge.jpeg'
+  },
+  {
+    name: 'Piazza Pretoria',
+    link: './images/element-piazza-pretoria.jpeg'
+  }
+];
+
+function getCard(cardSrc, cardName) {
+  const elementElement = elementTemplate.querySelector('.element').cloneNode(true);
+  const elementImage = elementElement.querySelector('.element__image');
+  const elementTitle = elementElement.querySelector('.element__title');
+  const trashButton = elementElement.querySelector('.element__trash');
+  const likeButton = elementElement.querySelector('.element__like');
+  elementImage.src = cardSrc;
+  elementTitle.textContent = cardName;
+  trashButton.addEventListener('click', function (evt) {
+    evt.target.closest('.element').remove();
+  } );
+  likeButton.addEventListener('click', function (evt) {
+    evt.target.classList.toggle('element__like_active');
+  });
+  elementImage.addEventListener('click', function() {
+    popupPhotoElement.querySelector('.popup__img').src = cardSrc;
+    popupPhotoElement.querySelector('.popup__caption').textContent = cardName;
+    popupPhotoElement.querySelector('.popup__img').alt = cardName;
+    container.append(popupPhotoElement);
+    popupOpen(popupPhotoElement);
+  });
+  return elementElement;
+}
+
+//Рендеринг массива из 6-ти карточек
+initialCards.forEach((item) => {
+  elementContainer.append(getCard(item.link, item.name));
+});
+
+function addFormSubmitHandler(evt) {  //Добавление новой карточки
+  evt.preventDefault();
+  popupClose(popupAdd);
+  elementContainer.prepend(getCard(popupAdd.querySelector('input[name="link"]').value, popupAdd.querySelector('input[name="place-name"]').value));
+}
+
+popupAddForm.addEventListener('submit', addFormSubmitHandler); //Обработчик клика при добавлении новой карточки
+
 
 function popupFormOpen() {
   popupName.value = profileName.textContent;
   popupHobby.value = profileHobby.textContent;
 }
 
-function popupOpenClose(popup) {
-  popup.classList.toggle('popup_opened');
+function popupOpen(popup) {
+  popup.classList.remove('fade-out');
+  popup.classList.add('fade-in');
 }
 
-function formSubmitHandler(evt) {
+function popupClose(popup) {
+  popup.classList.remove('fade-in');
+  popup.classList.add('fade-out')
+}
+
+function editFormSubmitHandler(evt) {
   evt.preventDefault();
-  popupOpenClose(popupEdit);
+  popupClose(popupEdit);
   profileName.textContent = popupName.value;
   profileHobby.textContent = popupHobby.value;
 }
 
-function addLike(evt) {
-  evt.target.classList.toggle('element__like_active');
-}
+addButton.addEventListener('click', function () {  //Обработчик открытия попапа для добавления карточки
+  popupOpen(popupAdd);
+});
+
+popupAddBtnClose.addEventListener('click', function () { //Обработчик закрытия попапа для добавления карточки
+  popupClose(popupAdd);
+});
 
 editButton.addEventListener('click', function () {
   popupFormOpen();
-  popupOpenClose(popupEdit);
+  popupOpen(popupEdit);
 });
 
 popupEditBtnClose.addEventListener('click', function () {
-  popupOpenClose(popupEdit);
+  popupClose(popupEdit);
 });
 
+popupEditForm.addEventListener('submit', editFormSubmitHandler);
 
-
-popupForm.addEventListener('submit', formSubmitHandler);
-
-likeElementButtons.forEach((item) => {
-  item.addEventListener('click', addLike);
-});
-
-elementImages.forEach((item) => {
-  item.addEventListener('click', function () {
-    popupPhotoElement.querySelector('.popup__img').src = item.src;
-    popupPhotoElement.querySelector('.popup__img').alt = item.alt;
-    popupPhotoElement.querySelector('.popup__caption').textContent = item.nextSibling.nextSibling.textContent;
-    container.append(popupPhotoElement);
-    popupOpenClose(popupPhotoElement);
-  });
-});
 
 popupPhotoBtnClose.addEventListener('click', function () {
-  popupOpenClose(popupPhotoElement);
-})
+  popupClose(popupPhotoElement);
+});
+
+
+
+
